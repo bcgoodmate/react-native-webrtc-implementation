67400631f12e24e14848a6164fb1189c833e8664
Kirk Alvar <genesis.revelation89@gmail.com>
2020-04-03 08:04:23 +0800

    Update webrtcWatch.js
    Code cleanup

--- samples/webrtcWatch.js
+++ samples/webrtcWatch.js
@@ -1,7 +1,7 @@
 /*
   For node server use https://github.com/TannerGabriel/WebRTC-Video-Broadcast
   - run node server
-  - open https://lyb-streaming.herokuapp.com/broadcast.html or http://localhost:3000/broadcast.html
+  - open http://localhost:3000/broadcast.html
 
   To deploy node.js project on Heroku watch this youtube video https://www.youtube.com/watch?v=MxfxiR8TVNU
 
@@ -28,7 +28,7 @@ const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 export default WebRTCWatch = _ => {  
-  let peer, socket;
+  let socket, peer;
   let config = {
     iceServers: [
       {urls: 'stun:stun.services.mozilla.com'},
@@ -39,47 +39,20 @@ export default WebRTCWatch = _ => {
   const [remoteStream, setRemoteStream] = useState();
  
   const watch = _ => {
-    socket = io('https://lyb-streaming.herokuapp.com'); // http://192.168.xxx.xxx:3000 run cmd command ipconfig
-
+    socket = io('http://192.168.xxx.xxx:3000'); 
     socket
-      .on('connect', _ => {
-        socket.emit('watcher'); 
-      })
+      .on('connect', _ => socket.emit('watcher'))
       .on('offer', async (id, desc) => {
-        try {
-          peer = new RTCPeerConnection(config);
-
-          peer
-            .setRemoteDescription(new RTCSessionDescription(desc))
-            .then(_ => peer.createAnswer())
-            .then(sdp => peer.setLocalDescription(sdp))
-            .then(_ => { 
-              socket.emit('answer', id, peer.localDescription); 
-            });
-
-          peer.onicecandidate = e => {
-            if (e.candidate) {
-              socket.emit('candidate', id, e.candidate);
-            }
-          }; 
-
-          // onaddstream is equivalent to ontrack 
-          peer.onaddstream = e => { 
-            if (e.stream && remoteStream !== e.stream) {
-              setRemoteStream(e.stream);
-            }
-          };  
-        } catch (e) {
-          console.error(e);
-        }
+        peer = new RTCPeerConnection(config);
+        peer
+          .setRemoteDescription(new RTCSessionDescription(desc))
+          .then(_ => peer.createAnswer())
+          .then(sdp => peer.setLocalDescription(sdp))
+          .then(_ => socket.emit('answer', id, peer.localDescription));
+        peer.onicecandidate = e => e.candidate && socket.emit('candidate', id, e.candidate);
+        peer.onaddstream = e => e.stream && remoteStream !== e.stream && setRemoteStream(e.stream); 
       })
-      .on('candidate', (id, candidate) => {
-        try {
-          peer.addIceCandidate(new RTCIceCandidate(candidate));
-        } catch (err) {
-          console.log('Error adding iceCandidate', err);
-        }
-      })
+      .on('candidate', (id, candidate) => peer.addIceCandidate(new RTCIceCandidate(candidate)))
       .on('disconnect', _ => {
         peer.close();
         socket.disconnect(true);
