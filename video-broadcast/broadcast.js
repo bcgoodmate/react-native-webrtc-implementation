c3ac94e40e6259254c36ef496facd52cd649d6e0
Kirk Alvar <genesis.revelation89@gmail.com>
2020-04-11 21:50:22 +0800

    Exchange file content.
    Exchange file content.

--- video-broadcast/broadcast.js
+++ video-broadcast/broadcast.js
@@ -1,75 +1,77 @@
 /*
   For node server use https://github.com/TannerGabriel/WebRTC-Video-Broadcast
   - run node server
-  - open http://localhost:3000/broadcast.html
+  - open http://localhost:3000/watch.html
 
   To deploy node.js project on Heroku watch this youtube video https://www.youtube.com/watch?v=MxfxiR8TVNU
-
-  General Flow:
-  - socket on connection
-  - socket emit watcher
-  - socket on offer
-    + RTCPeerConnection
-    + setRemoteDescription
-    + createAnswer
-    + setLocalDescription then emit answer
-    + onaddstream
-    + onicecandidate
-  - socket on candidate
-  - socket on disconnectPeer
 */
 
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, Dimensions, View} from 'react-native';
-import {RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, RTCView} from 'react-native-webrtc';
+import React, {useState} from 'react';
+import {StyleSheet, Dimensions, View, Button} from 'react-native';
+import {RTCPeerConnection, RTCSessionDescription, mediaDevices, RTCView} from 'react-native-webrtc';
 import io from 'socket.io-client';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
-export default WebRTCWatch = _ => {  
+export default WebRTCBroadcast = _ => {  
   let socket, peer;
-  let config = {
+  let clients = {};
+
+  const config = {
     iceServers: [
       {urls: 'stun:stun.services.mozilla.com'},
       {urls: 'stun:stun.l.google.com:19302'}
     ]
   };
-   
-  const [remoteStream, setRemoteStream] = useState();
- 
-  useEffect(_ => {
+  const [localStream, setLocalStream] = useState();
+
+  const broadcast = async _ => {
+    const stream = await mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+    }); 
+    
     socket = io('http://192.168.xxx.xxx:3000'); 
     socket
-      .on('connect', _ => socket.emit('watcher'))
-      .on('offer', async (id, desc) => {
+      .emit('broadcaster')
+      .on('watcher', id => {
         peer = new RTCPeerConnection(config);
+        clients[id] = peer;
+        peer.addStream(stream); 
         peer
-          .setRemoteDescription(new RTCSessionDescription(desc))
-          .then(_ => peer.createAnswer())
+          .createOffer()
           .then(sdp => peer.setLocalDescription(sdp))
-          .then(_ => socket.emit('answer', id, peer.localDescription));
-        peer.onicecandidate = e => e.candidate && socket.emit('candidate', id, e.candidate);
-        peer.onaddstream = e => e.stream && remoteStream !== e.stream && setRemoteStream(e.stream); 
+          .then(_ => socket.emit('offer', id, peer.localDescription));
+        peer.onicecandidate = e => e.candidate && socket.emit('candidate', id, e.candidate); 
       })
-      .on('candidate', (id, candidate) => peer.addIceCandidate(new RTCIceCandidate(candidate)))
-      .on('disconnectPeer', _ => {
-        peer.close();
-        socket.disconnect(true);
+      .on('answer', (id, desc) => {
+        clients[id].setRemoteDescription(new RTCSessionDescription(desc));
+      })
+      .on('disconnectPeer', id => {
+        const client = clients[id];
+ 
+        if(client) {
+          client.close();
+          delete clients[id];
+        }
       });
-  }, []);
+
+    setLocalStream(stream);
+  };
 
   return (
     <View style={{flex: 1}}>
-      {remoteStream && <RTCView 
+      {localStream && <RTCView 
+        streamURL={localStream.toURL()}
         zIndex={0} 
-        streamURL={remoteStream.toURL()}
         objectFit={'cover'} 
-        style={styles.fullScreen} />}  
+        style={styles.fullScreen}
+      /> || <Button title="Broadcast Now" onPress={_ => broadcast()} />} 
     </View> 
   );
 }
-
+ 
 const styles = StyleSheet.create({
   fullScreen: {
     position: 'absolute',
